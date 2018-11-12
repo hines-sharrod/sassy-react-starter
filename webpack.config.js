09618bdedfaf1fed7d08c1f792519825f95ae180
Sharrod Hines <sharrodhines@gmail.com>
2018-11-12 00:39:59 -0500

    set up the app entry point app, added the html webpack plugin, built index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,31 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
         }
-      ]
-    }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      // Where to find the HTML file to inject the bundle into
+      template: "./src/index.html",
+      // The filename is the name the dist folder will name the outputted file
+      filename: "./index.html"
+    })
+  ]
 };
